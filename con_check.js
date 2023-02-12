842efebb0b9292da744c0a380d366e6e59f46eb7
Den-Med <124715785+Den-Med@users.noreply.github.com>
2023-02-12 17:52:39 +0300

    Update con_check.js

--- con_check.js
+++ con_check.js
@@ -1,14 +1,27 @@
-let content = `
-              <form>
-                 <div class="form-group">
-                    <input type="number" id="damage" name="damage" size="4" placeholder="Введите полученный урон">
-                 </div>
-                 <div class="form-group">
-                    <input type="number" id="damage" name="damage" size="4" placeholder="Ситуационный бонус, на пример 1d4">
-                 </div>
-              </form>
-              `;
+path = `systems/dnd5e/templates/chat/roll-dialog.hbs`;
+defaultRollMode = `publicroll`;
+chooseModifier  = false;
+tttt = `Hellow World`
 
+prf_cont = `<form>
+     <div class=\"form-group\">
+         <label>Урон:</label>
+         <input type="number" id="damage" name="damage" size="4" placeholder="полученный урон">
+     </div>
+          `
+
+    const cont_suf = await renderTemplate(path , {
+      formula: ` + @bonus`,
+      defaultRollMode,
+      rollModes: CONFIG.Dice.rollModes,
+      chooseModifier,
+
+      abilities: CONFIG.DND5E.abilities
+    });
+
+content = prf_cont + cont_suf.substr(8)
+
+confirmed = false;
 new Dialog({
     title: "Проверка концентрации",
     content: content,
@@ -20,10 +33,11 @@ new Dialog({
   
    close: html => {
        (async () => {
-                   dmg_val = html.find('#damage')[0].value
-                   if (confirmed) { chat_text = dmg_val 
+                   dmg_val = html.find('#damage')[0].value;
+                   console.log(tttt)
+                   if (confirmed && dmg_val > 0) { chat_text = dmg_val 
                                    ChatMessage.create({ content : chat_text })
-                                  }
+                                  };
                              
            
 
