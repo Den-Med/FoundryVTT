1aa6998e2a108db220c99f2600e1f558f9acfaa0
Den-Med <124715785+Den-Med@users.noreply.github.com>
2023-02-12 19:26:36 +0300

    Update con_check.js

--- con_check.js
+++ con_check.js
@@ -2,45 +2,43 @@ path = `systems/dnd5e/templates/chat/roll-dialog.hbs`;
 defaultRollMode = `publicroll`;
 chooseModifier  = false;
 tttt = `Hellow World`
-
 prf_cont = `<form>
      <div class=\"form-group\">
          <label>Урон:</label>
          <input type="number" id="damage" name="damage" size="4" placeholder="полученный урон">
      </div>
           `
+// ======================================
 
-    const cont_suf = await renderTemplate(path , {
-      formula: ` + @bonus`,
-      defaultRollMode,
-      rollModes: CONFIG.Dice.rollModes,
-      chooseModifier,
-
-      abilities: CONFIG.DND5E.abilities
-    });
-
-content = prf_cont + cont_suf.substr(8)
+const cont_suf = await renderTemplate(path , {
+ formula: ` + @bonus`,
+ defaultRollMode,
+ rollModes: CONFIG.Dice.rollModes,
+ chooseModifier,
+ abilities: CONFIG.DND5E.abilities
+});
+content = prf_cont + cont_suf.substr(8);
+// ====================================
 
 confirmed = false;
 new Dialog({
     title: "Проверка концентрации",
     content: content,
     buttons: {
-               button1: { label: "button1", callback: () => confirmed = true },
-               button2: { label: "button2", callback: () => confirmed = false }
+               advantage: {    label: game.i18n.localize("DND5E.Advantage"),    callback: () => confirmed = 'a' },
+               normal: {       label: game.i18n.localize("DND5E.Normal"),       callback: () => confirmed = 'n' },
+               disadvantage: { label: game.i18n.localize("DND5E.Disadvantage"), callback: () => confirmed = 'd' },
     },
 
-  
    close: html => {
        (async () => {
                    dmg_val = html.find('#damage')[0].value;
-                   console.log(tttt)
-                   if (confirmed && dmg_val > 0) { chat_text = dmg_val 
-                                   ChatMessage.create({ content : chat_text })
-                                  };
-                             
-           
-
+                   console.log(tttt + 1)
+                   test_fun()
        })();
    }
 }).render(true);
+
+console.log(tttt + 2);
+
+function test_fun() {console.log(tttt + 3)};
