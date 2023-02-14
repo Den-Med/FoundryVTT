aaedd23253263bba0ced1537b5a83c80c1cc651f
Den-Med <124715785+Den-Med@users.noreply.github.com>
2023-02-14 23:27:49 +0300

    Update con_check.js

--- con_check.js
+++ con_check.js
@@ -5,26 +5,19 @@ if (!c_token.controlled) {
 };
 save_bonus = c_token.actor.system.abilities.con.save;
 path = `systems/dnd5e/templates/chat/roll-dialog.hbs`;
-defaultRollMode = `publicroll`;
-chooseModifier  = false;
-tttt = `Hellow World`;
-prf_cont = `<form>
-     <div class=\"form-group\">
-         <label>Урон:</label>
-         <input type="number" name="damage" min="1" placeholder="полученный урон">
-     </div>
-          `;
-suf_cont = `</form>`;
 // ======================================
 
 const cont = await renderTemplate(path , {
  formula: `1d20 + ` + save_bonus + ` + @bonus`,
- defaultRollMode,
  rollModes: CONFIG.Dice.rollModes,
- chooseModifier,
- abilities: CONFIG.DND5E.abilities
 });
-content = prf_cont + cont.slice(8, -368) + suf_cont;
+var cont_elem = document.createElement(`form`);
+cont_elem.innerHTML = cont;
+var v_div = document.createElement(`div`);
+v_div.classList.add(`form-group`);
+v_div.innerHTML = '<label>Урон:</label>\n<input type="number" name="damage" min="1" placeholder="полученный урон">';
+cont_elem.insertBefore(v_div, cont_elem.childNodes[0])
+content = cont_elem.outerHTML;
 // ====================================
 
 confirmed = false;
@@ -41,46 +34,49 @@ new Dialog({
        (async () => {
             const form = html[0].querySelector("form");
             dmg_val = form.damage.value;
+            rollMode = form.rollMode.value;
             if (dmg_val < 1) { 
                  ui.notifications.warn('Уронне может быть меньше 1');
                  return; 
             };
             con_bonus = form.bonus.value;
-            con_roll(dmg_val, confirmed, con_bonus)
+            con_roll(dmg_val, confirmed, con_bonus, rollMode)
        })();
    }
 }).render(true);
 
-function con_roll(dmg, mod, bon) {
+function con_roll(dmg, mod, bon, rmod) {
      target_val = Math.max(10, dmg);
      switch (mod) {
           case 1: //Advantage
                modif = [`kh`];
-               num_d = 2;
+//               num_d = 2;
                break;
           case 0: //Normal
                modif = undefined;
-               num_d = 1;
+//               num_d = 1;
                break;
           case -1: //Disadvantage
                modif = [`kl`];
-               num_d = 2;
+//               num_d = 2;
                break;
           default:
                console.error(`con_check.js - callback error`)
                return;
      };
+     if (!modif) {num_d = 2} else {num_d = 1};
      const die = new Die({number: num_d, faces: 20, modifiers: modif,
                     options: { target: target_val },
                    });
-     const roll = Roll.fromTerms([die]);
+     const roll = new Roll.fromTerms([die]);
      const save_b = new Roll(`+` + save_bonus);
      const bonus_roll = new Roll(bon);
      if ( !(bonus_roll.terms[0] instanceof OperatorTerm) && bonus_roll.terms[0] ) save_b.terms.push(new OperatorTerm({operator: "+"}));
      roll.terms = roll.terms.concat(save_b.terms, bonus_roll.terms);
      roll._formula = roll.constructor.getFormula(roll.terms);
-     descr = `Проверка концентрации, при уроне ` + target_val;
-     roll.toMessage({
-          speaker: ChatMessage.getSpeaker(),
-          flavor: descr});
+     descr = `Проверка концентрации. Сложность: ` + target_val;
+     roll.toMessage({speaker: ChatMessage.getSpeaker(),
+                     flavor: descr},
+                    {rollMode: rmod} 
+                   );
 };
