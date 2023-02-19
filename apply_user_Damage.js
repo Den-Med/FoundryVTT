0fec2a78a6bacac588c84d7d50333807e9f19748
Den-Med <124715785+Den-Med@users.noreply.github.com>
2023-02-19 04:06:54 +0300

    Update apply_user_Damage.js

--- apply_user_Damage.js
+++ apply_user_Damage.js
@@ -1,6 +1,7 @@
 
 const dmg_txt = game.i18n.localize('DND5E.Damage');
-let damage = {dmg1: "", dmg2: "", dmg3: ""};
+const dmg = {v: '', m: '', t: ''}
+let damage = {dmg1: deepClone(dmg), dmg2: deepClone(dmg), dmg3: deepClone(dmg), dmg_total: 0};
 
 let dialogDamge = new Dialog({
   title: dmg_txt,
@@ -9,13 +10,13 @@ let dialogDamge = new Dialog({
     calc_b: {
       label: 'Рассчитать урон',
       callback: () => {
-//        ====
-        const form = document.querySelector("#dmg_apply");
-        damage.dmg1 = form.damage1.value;
-        damage.dmg2 = form.damage2.value;
-        damage.dmg3 = form.damage3.value;
+        const dmg_form = document.querySelectorAll("#dmg_apply input");
+        dmg_form.forEach((node_d)=>{
+          if (node_d.name in damage) damage[node_d.name].v = node_d.value
+        });
         dialogDamge.data.content = create_con();
-        console.log(dmg1+dmg2+dmg3);
+        damageCalc()
+        console.log(damage);
         dialogDamge.render(true);
       }
     },
@@ -53,9 +54,31 @@ function create_con() {
   </div>
   <div class="form-group">
     <label style="flex: 1">${game.i18n.localize('DND5E.PropertyTotal')}:</label>
-    <input type="text" name="dmg_total" value="1" disabled/>
+    <input type="text" name="dmg_total" value=${damage.dmg_total} disabled/>
     <strong> </strong>
   </div>
 </form>`;
   return cont_l;
+};
+
+function damageCalc() {
+  if (damage.dmg1.v != damage.dmg1.m){
+    dmg_r = new Roll(damage.dmg1.v);
+    dmg_r.evaluate({async: false});
+    damage.dmg1.t = parseInt(dmg_r._total);
+    damage.dmg1.m = damage.dmg1.v;
+  };
+  if (damage.dmg2.v != damage.dmg2.m){
+    dmg_r = new Roll('(' + damage.dmg2.v + ')/2');
+    dmg_r.evaluate({async: false});
+    damage.dmg2.t = parseInt(dmg_r._total);
+    damage.dmg2.m = damage.dmg2.v;
+  };
+  if (damage.dmg3.v != damage.dmg3.m){
+    dmg_r = new Roll('(' + damage.dmg3.v + ')/4');
+    dmg_r.evaluate({async: false});
+    damage.dmg3.t = parseInt(dmg_r._total);
+    damage.dmg3.m = damage.dmg3.v;
+  };
+  damage.dmg_total = damage.dmg1.t + damage.dmg2.t + damage.dmg3.t;
 };
