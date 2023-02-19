ddeec053b52f2327f90ca36ab90aa5e233749469
Den-Med <124715785+Den-Med@users.noreply.github.com>
2023-02-19 04:50:50 +0300

    Update apply_user_Damage.js

--- apply_user_Damage.js
+++ apply_user_Damage.js
@@ -11,9 +11,9 @@ let dialogDamge = new Dialog({
       label: 'Рассчитать урон',
       callback: () => {
         const dmg_form = document.querySelectorAll("#dmg_apply input");
-        dmg_form.forEach((node_d)=>{
+        for (let node_d of dmg_form){
           if (node_d.name in damage) damage[node_d.name].v = node_d.value
-        });
+        };
         dialogDamge.data.content = create_con();
         damageCalc()
         console.log(damage);
@@ -23,9 +23,7 @@ let dialogDamge = new Dialog({
     applyDmg: {
       label: game.i18n.localize('DND5E.ChatContextDamage'),
       callback: () => {
-//        _token.actor.update(updates, {dhp: -10});
-//        dialogDamge.render(true);
-
+        dialogDamge.render(true);
       }
     },
   },
@@ -39,18 +37,18 @@ function create_con() {
   cont_l = `<form id="dmg_apply">
   <div class="form-group">
       <label style="flex: 1">${dmg_txt} 1:</label>
-      <input type="text" name="dmg1" value=${damage.dmg1 || "''"} />
-      <strong ></strong>
+      <input type="text" name="dmg1" value=${damage.dmg1.v || "''"} />
+      <strong>${' =' + damage.dmg1.t}</strong>
   </div>
   <div class="form-group">
     <label style="flex: 1">${dmg_txt} 2:</label>
-    <input type="text" name="dmg2" value=${damage.dmg2 || "''"} />
-    <strong> /2</strong>
+    <input type="text" name="dmg2" value=${damage.dmg2.v || "''"} />
+    <strong>${' /2 = ' + damage.dmg2.t}</strong>
   </div>
   <div class="form-group">
     <label style="flex: 1">${dmg_txt} 3:</label>
-    <input type="text" name="dmg3" value=${damage.dmg3 || "''"} />
-    <strong>/4</strong>
+    <input type="text" name="dmg3" value=${damage.dmg3.v || "''"} />
+    <strong>${' /4 = ' + damage.dmg3.t}</strong>
   </div>
   <div class="form-group">
     <label style="flex: 1">${game.i18n.localize('DND5E.PropertyTotal')}:</label>
@@ -62,23 +60,21 @@ function create_con() {
 };
 
 function damageCalc() {
-  if (damage.dmg1.v != damage.dmg1.m){
-    dmg_r = new Roll(damage.dmg1.v);
-    dmg_r.evaluate({async: false});
-    damage.dmg1.t = parseInt(dmg_r._total);
-    damage.dmg1.m = damage.dmg1.v;
-  };
-  if (damage.dmg2.v != damage.dmg2.m){
-    dmg_r = new Roll('(' + damage.dmg2.v + ')/2');
-    dmg_r.evaluate({async: false});
-    damage.dmg2.t = parseInt(dmg_r._total);
-    damage.dmg2.m = damage.dmg2.v;
-  };
-  if (damage.dmg3.v != damage.dmg3.m){
-    dmg_r = new Roll('(' + damage.dmg3.v + ')/4');
-    dmg_r.evaluate({async: false});
-    damage.dmg3.t = parseInt(dmg_r._total);
-    damage.dmg3.m = damage.dmg3.v;
-  };
+  rollDamage(damage.dmg1, '');
+  rollDamage(damage.dmg2, '/2');
+  rollDamage(damage.dmg3, '/4');
   damage.dmg_total = damage.dmg1.t + damage.dmg2.t + damage.dmg3.t;
+};
+
+function rollDamage(i_dmg, mod){
+  if (!i_dmg.v) {
+    i_dmg.t = 0;
+    i_dmg.m = i_dmg.v;
+  } else if (i_dmg.v != i_dmg.m) {
+    formula = mod ? '(' + i_dmg.v + ')' + mod : i_dmg.v;
+    dmg_r = new Roll(formula);
+    dmg_r.evaluate({async: false});
+    i_dmg.t = parseInt(dmg_r._total);
+    i_dmg.m = i_dmg.v;
+  };
 };
